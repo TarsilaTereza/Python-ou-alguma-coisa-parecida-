2e6d77b34430f307feb783c341ddfb31dfcef4fb
tarsilatereza <tarsilaterezaprm@gmail.com>
2021-12-30 16:13:54 -0300

    complementando

--- classes/class2_tiposPrimitivos.py
+++ classes/class2_tiposPrimitivos.py
@@ -1,18 +1,22 @@
-# int - 12; 13; 9082; -9887876
-# float - 3.4; 9.8; 7.0
-# bool - True; False
-# string - "olá"; "aoba"; ""
+# EXEMPLOS
+n1 = input("type a number: ")
+n2 = input("type other number: ")
+s = n1 + n2
+print("the sume is ", s)
+#CONSOLE:
+#> type a number:2
+#> type other number3
+#> the sume is  23
 
-n1 = input('digite o valor')
-print(type(n1)) #vai retornar uma string. tipo primitivo não especificado.
+n1 = int(input("type a number: "))
+n2 = int(input("type other number: "))
+s = n1+ n2
+print("the sume is ", s)
+#CONSOLE
+#> type a number: 2
+#> type other number: 3
+#> the sume is  5
 
-n2 = int(input('type a value'))
-print(type(n2)) #retornou int.
-
-n3 = int(input('type other value'))
-
-s = n2 + n3
-print('the sum between', n2, "and", n3, 'is', s)
-
-#utilizando o .format()
-print('the sum between {} and {} is {}'.format(n2, n3, s))
+print("the sume between {} and {} is {}".format(n1, n2, s))
+#CONSOLE
+#> the sume between 2 and 3 is 5
