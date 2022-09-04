b5d886dde5e24d58d0a45ce6cb254c513fe309b5
tarsilatereza <tarsilaterezaprm@gmail.com>
2022-09-04 20:35:25 -0300

    exercicios do gunabara

---
+++ classes/desafios/06.py
@@ -0,0 +1,6 @@
+#algoritimo que lê número e dá o dobro, triplo e a raiz quadrada
+n1 = int(input('digite um número'))
+d = n1 * 2
+t = n1 * 3
+r = n1 * n1
+print('dado {}, o duplo é {}, o triplo é {} e a raiz quadrada é {}'.format(n1, d, t, r))        
